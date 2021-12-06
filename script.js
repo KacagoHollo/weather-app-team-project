ec7c550898eb0e995c8ee00eab051c2a54f3acc6
klikknpro <80091502+klikknpro@users.noreply.github.com>
2021-12-06 19:18:18 +0100

    Day 1
    - html id root tiszta
    - input change-re mukodik a listabol kiolvasas ... ize

--- script.js
+++ script.js
@@ -1,41 +1,71 @@
-function loadFunction(){
-
-    getWeather();
-}
-function render(dataObj){
-    const root = document.getElementById("root");
-    root.insertAdjacentHTML("beforeend", `    
-        <div class=>
-            <h2>"Időjárás"</h2>
+function loadFunction() {
+  const root = document.getElementById("root");
+  root.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <section class="top-banner">
+        <div class="container">
+            <h1 class="heading">Simple Weather App</h1>
+            <form class="ASD">
+                <input type="text" placeholder="Search for a city" list="cityname" autofocus />
+                <datalist id="cityname">
+                    <option value="Boston"></option>
+                    <option value="London"></option>
+                    <option value="Paris"></option>
+                    <option value="Budapest"></option>
+                    <option value="Sydney"></option>
+                </datalist>
+            </form>
         </div>
+    </section>
+    <section class="ajax-section">
+        <div class="container">
+            <ul class="cities"></ul>
+        </div>
+    </section>
+  `
+  );
 
-        <div class=></div>
-        <div class=></div>
-        <div class=></div>
+  const input = document.querySelector("input");
+  input.addEventListener("change", render);
+}
+
+async function render(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  const cityData = await getWeather(event.target.value);
+
+  const root = document.getElementById("root");
+  root.insertAdjacentHTML(
+    "beforeend",
+    `
+        <div class="city">
+            <h2>${cityData.city}</h2>
+        </div>
+        <div class="temp">
+            <p>${cityData.temp}</p>
+        </div>
+        <div class="sky">
+            <p>${cityData.sky}</p>
+        </div>
+        <div class="humidity">
+            <p>${cityData.humidity}</p>
+        </div>
         `
-    )
+  );
 }
 
-
-
-
-
-
-let city = "Paris";
-
-async function getWeather () {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=a1b06107fefa4c95a81102652210612&q=${city}&aqi=no`);
-    const data = await response.json();
-    useApiData(data);
+async function getWeather(selectedCity) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=a1b06107fefa4c95a81102652210612&q=${selectedCity}&aqi=no`
+  );
+  const data = await response.json();
+  let dataObj = {
+    city: data.location.name,
+    temp: data.current.temp_c,
+    sky: data.current.condition.text,
+    humidity: data.current.humidity,
+  };
+  return dataObj;
 }
-function useApiData(data){
-    let dataObj ={
-        city: data.location.name,
-        temp: data.current.temp_c,
-        sky: data.current.condition.text,
-        hum : data.current.humidity
-    }
-    console.log(dataObj);
-    render(dataObj);
-}
-window.addEventListener("load", loadFunction());
+window.addEventListener("load", loadFunction);
